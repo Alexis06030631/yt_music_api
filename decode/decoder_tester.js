6e39c422b50610a903ebb163ed8d67e47fab636a
Alexis <61119747+Alexis06030631@users.noreply.github.com>
2025-03-19 18:06:08 +0100

    add Decoder test action GitHub

---
+++ decode/decoder_tester.js
@@ -0,0 +1,34 @@
+const ytb = require('../dist')
+const path = require("node:path");
+const core = require('@actions/core');
+const {readFileSync} = require("node:fs");
+
+
+process.env.YT_DEBUG_MODE = true
+const file = readFileSync(path.resolve(__dirname, 'build/decoder.js')).toString()
+process.env.buildDevDecoderFile = file
+core.info("Decoder truncated: " + file.substring(0, 50))
+
+
+ytb.getPlayers('Zec28Gd5fls').then(async (data) => {
+	let validUrlCount = 0
+
+	data.audios.push(...data.videos)
+	for (let i = 0; i < data.audios.length; i++) {
+		await data.audios[i].url().then((url) => {
+			core.info(`itag: ${url.match(/itag=(\d+)/)[1]} passed`)
+			validUrlCount++
+		}).catch((e) => {
+			core.notice(`itag: ${e.details.url.match(/itag=(\d+)/)[1]} FAILED`)
+		})
+	}
+
+	if (validUrlCount === 0) {
+		core.setFailed("No valid URL found")
+	} else {
+		core.info("Test passed with " + validUrlCount + " valid URLs")
+	}
+}).catch((err) => {
+	core.setFailed(`An error occurred: ${err}`)
+	throw err
+})
